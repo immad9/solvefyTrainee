3dfb0443d5cfaf0f7df21320612233d790ee8460
ahramay <immadh9@gmail.com>
2025-03-08 16:24:12 +0500

    update versal.json file

--- app.js
+++ app.js
@@ -7,20 +7,14 @@ import authRouter from './routes/authRoutes.js'
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+// const allowedOrigins = ['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors
-  ({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-  }))
+app.use(cors())
 
 // API Endpoints
 app.get('/', (req, res) => res.send("API Working Fine"))
